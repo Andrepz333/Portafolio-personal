d1f2999f3f166593fe50583e85e381f67788b794
Andrepz333 <andreapz333@gmail.com>
2025-03-13 15:36:34 -0500

    actualización

--- Script.js
+++ Script.js
@@ -1,2 +1,20 @@
+function toggleSkill(card) {
+    card.classList.toggle("active");
+
+    let percent = card.getAttribute("data-percent") || 50;
+    let progressBar = card.querySelector(".progress-bar");
+    let percentageText = card.querySelector(".percentage");
+
+    // Cálculo del `stroke-dashoffset` según el porcentaje
+    let offset = 251.2 - (percent / 100) * 251.2;
+    progressBar.style.strokeDashoffset = offset;
+
+    // Mostrar el porcentaje en el centro
+    percentageText.textContent = `${percent}%`;
+}
+
+
+
+
 
 
